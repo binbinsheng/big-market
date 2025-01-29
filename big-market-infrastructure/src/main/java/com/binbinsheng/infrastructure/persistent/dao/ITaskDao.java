b32ebabb1dc5527770cf1e09fcaad11f9deeadb1
Bin <1447455148@qq.com>
2025-01-29 12:24:20 +0800

    feat:营销服务 第17节：用于领取活动库表设计

---
+++ big-market-infrastructure/src/main/java/com/binbinsheng/infrastructure/persistent/dao/ITaskDao.java
@@ -0,0 +1,10 @@
+package com.binbinsheng.infrastructure.persistent.dao;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 任务表，发送MQ
+ * @create 2024-04-03 15:57
+ */
+public interface ITaskDao {
+}
+
